8407c248eca3ae26bb366a9934d3f9cbb77f49cb
Kayw Lee <kwooler@gmail.com>
2024-04-02 09:35:48 -0700

    CS140 P1

---
+++ P1A1.asm
@@ -0,0 +1,16 @@
+# Title:  					Filename:
+# Author:  				Date:
+# Description:
+# Input:
+# Output:
+################# Data segment #####################
+.data
+
+
+
+################# Code segment #####################
+.text
+.globl main
+main:  	
+
+
